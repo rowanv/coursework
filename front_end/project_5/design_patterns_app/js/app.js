d1ed72a01305106ac1d2262e38d14f7f857f683a
rvasquez <rvasquez@twitter.com>
2015-08-11 14:54:31 -0500

    App works for n cats

--- front_end/project_5/design_patterns_app/js/app.js
+++ front_end/project_5/design_patterns_app/js/app.js
@@ -1,38 +1,41 @@
-	'use strict';
+'use strict';
 
 
-	function CatCount (pic_url, cat_name){
-		this.pic_url = pic_url;
-		this.click_count = 0;
-		this.cat_name = cat_name;
-		return false;
-
-	}
-
-	/* Populate allCats with the cats that will be shown on the page */
-
-	var allCats = [];
-
-	var sleepyCat = new CatCount('http://4.bp.blogspot.com/_RAIC5wwyFQg/S7L9XSPu_6I/AAAAAAAAAAc/GyKdia77ke8/s200/cat.jpg', 'Sleepy Cat');
-	allCats.push(sleepyCat);
-	var winkingCat = new CatCount('https://pbs.twimg.com/profile_images/2167035896/123cat.jpg', 'Winking Cat');
-	allCats.push(winkingCat);
-
-	var cat_name;
-	var pic_url;
-	var click_count;
-
-	function catClick(element_id_to_be_replaced){
-	    var cat_click_p = document.getElementById(element_id_to_be_replaced);
-	    var number = cat_click_p.innerHTML;
-	    number 	= parseInt(number) + 1;
-	    cat_click_p.innerHTML = number;
-
-	}
 
 
-	var catGrid = document.getElementById('cat_grid');
-	catGrid.addEventListener('click', catClick.bind(this, 'number_clicked_0'), false);
+function CatCount (pic_url, cat_name){
+	this.pic_url = pic_url;
+	this.click_count = 0;
+	this.cat_name = cat_name;
+	return false;
+
+}
+
+/* Populate allCats with the cats that will be shown on the page */
+
+var allCats = [];
+
+var sleepyCat = new CatCount('http://4.bp.blogspot.com/_RAIC5wwyFQg/S7L9XSPu_6I/AAAAAAAAAAc/GyKdia77ke8/s200/cat.jpg', 'Sleepy Cat');
+allCats.push(sleepyCat);
+var winkingCat = new CatCount('https://pbs.twimg.com/profile_images/2167035896/123cat.jpg', 'Winking Cat');
+allCats.push(winkingCat);
+var musicCat = new CatCount('http://thepetproductguru.com/wp-content/uploads/2012/05/CAT-HEADPHONES.jpeg', 'Music Cat');
+allCats.push(musicCat);
+
+
+var cat_name;
+var pic_url;
+var click_count;
+
+/* Add listener for cat click counts
+*/
+
+function catClick(Cat, element_id_to_be_replaced){
+    var cat_click_p = document.getElementById(element_id_to_be_replaced);
+    Cat.click_count += 1;
+    cat_click_p.innerHTML = Cat.click_count;
+
+}
 
 
 
@@ -55,11 +58,11 @@ function initialAppendCats (){
 	var click_count_string = 'number_clicked_' + String(i);
 	$('.cat').append($('<div>', {text: 'This cat has been clicked'}));
 	$('.cat').append($('<p>', {text: 0, id: click_count_string}));
-	$('.cat').append($('<div>', {text: 'times.'}));
-	//allCats[i].prototype.trackClicks();
+	$('.cat').append($('<div>', {text: 'times. '}));
+	$('.cat').append($('<p>')); //newline
 
 	var catPic 	= document.getElementById(cat_name);
-	catPic.addEventListener('click', catClick.bind(this, click_count_string), false);
+	catPic.addEventListener('click', catClick.bind(this, allCats[i], click_count_string), false);
 	}
 
 
@@ -69,47 +72,4 @@ function initialAppendCats (){
 
 
 initialAppendCats();
-/*
-CatCount.prototype.trackClicks = function() {
-	$('.cat').click(function () {
-		console.log('Clicked');
-		this.click_count += 1;
-		document.getElementById('number_clicked').innerHTML = this.clickCount;
-	}
-	);
-
-}
-
-
-
-
-
-function updateCats(){
-	for (var i=0; i < allCats.length; i++){
-		click_count = allCats[i].click_count;
-		var click_count_string = 'number_clicked_' + String(i)
-		var click_count_string_hashtag = '#' + click_count_string
-		$(click_count_string_hashtag).replaceWith($('<div>', {text: click_count, id: click_count_string}))
-	}
-}
-
-initialAppendCats();
-
-var catGrid = document.getElementById('cat_grid');
-catGrid.addEventListener('click', updateCats, false);
-*/
-/*
-(function() {
-	var catClickCounter = 0;
-
-	$('.cat').click(function () {
-		console.log('Clicked');
-		catClickCounter += 1;
-		document.getElementById('number_clicked').innerHTML = catClickCounter;
-	}
-	);
-
-})
-();*/ 
-
 
