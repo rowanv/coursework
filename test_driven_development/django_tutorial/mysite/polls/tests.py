c639dc99e6b115fa1bdf47bb45929bb01e630a81
rowanv <rrvspam@gmail.com>
2015-08-16 11:59:41 -0500

    Refactoring tests

--- test_driven_development/django_tutorial/mysite/polls/tests.py
+++ test_driven_development/django_tutorial/mysite/polls/tests.py
@@ -21,7 +21,7 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
 
-	def test_home_page_can_save_a_post_request(self):
+	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
 		request.method = 'POST'
 		request.POST['item_text'] = 'A new question'
