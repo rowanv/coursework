e967a99e84dff1d882308e1a9e0dce78886aaca7
rowanv <rrvspam@gmail.com>
2015-08-16 08:36:53 -0500

    Added comments to describe tests necessary for minimum polling app

--- test_driven_development/django_tutorial/mysite/functional_tests.py
+++ test_driven_development/django_tutorial/mysite/functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+import unittest
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django in browser.title'
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Our user is interested in starting a poll with our new polling app. They
+		#navigate to its home page.
+
+		self.browser.get('http://localhost:8000')
+
+		#They notice a page title and header mentioning polls.
+		self.assertIn('Polling', self.browser.title)
+		self.fail('Finish the test!')
+
+#they are invited to enter a question.
+
+#Our user creates a new question
+
+#And now we see that the page lists the question as a poll.
+
+#We check that wse have saved the Question object in teh database
+
+#The question must have certain attributes: a publication date, and a question text
+
+#When we change the question's attributes, these are reflected in our app.
+
+
+
+if __name__ == '__main__':
+	unittest.main()
