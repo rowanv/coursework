f59482c679215b4669f685bff66f770842a40a3e
rowanv <rrvspam@gmail.com>
2015-08-18 20:39:51 -0500

    Minor linting for FT

--- test_driven_development/part_2/superlists/functional_tests/tests.py
+++ test_driven_development/part_2/superlists/functional_tests/tests.py
@@ -6,12 +6,12 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(StaticLiveServerTestCase):
 
     @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
+    def setUpClass(cls):  #1
+        for arg in sys.argv:  #2
+            if 'liveserver' in arg:  #3
+                cls.server_url = 'http://' + arg.split('=')[1]  #4
+                return  #5
+        super().setUpClass()  #6
         cls.server_url = cls.live_server_url
 
     @classmethod
